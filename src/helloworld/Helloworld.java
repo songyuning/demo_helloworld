dfc73e4cc89443bf86b4a923ff5fac8c158d9ce4
songyuning <songyuning@live.cn>
2015-12-28 20:30:40 +0800

    再试一遍"commit and push"。

--- src/helloworld/Helloworld.java
+++ src/helloworld/Helloworld.java
@@ -6,6 +6,7 @@ public class Helloworld {
 		// TODO Auto-generated method stub
 		System.out.println("Hello,world!");
 		System.out.println("Hello,eclipse!");
+		System.out.println("Oh,my god!");
 	}
 
 }
